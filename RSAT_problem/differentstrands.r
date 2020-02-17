8b289daedf3fd21db36aa152c7cc7d2dcd3cba48
Aida Ghayour <aghayourkhia@mun.ca>
2020-02-17 18:32:20 -0330

    Update differentstrands.r

--- RSAT_problem/differentstrands.r
+++ RSAT_problem/differentstrands.r
@@ -22,3 +22,19 @@ binMap.cebpb <- cut( cebpb_box$pos, breaks = seq(-200,0, by = 5), labels = seq(-
 boxplot(cebpb_box$pval~binMap.cebpb,ylab="-log10 Pval",xaxt="n",ylim=c(3,8),col = rgb(red = 0, green = 0, blue = 1, alpha = 0.3),add = TRUE, lty=1)
 
 legend(0,7, c("Default","reverse compliment","+ : The compliment", "O : Default", ".... : The compliment", "____ : Defualt"),lty=c(1,1), lwd=c(2.5,2.5),col=c("blue","red","black","yellow","green","orange"),density = 20,cex = 0.75)
+
+
+############################# Frequency
+
+cebpb <- read.delim("~/Documents/R/fpwm-Thesis-new/RSAT problem/Reverse complemetn/CEBPB JASPAR format (reverse complement).ft", header=FALSE, comment.char="#")
+reverse <- data.frame(cbind(cebpb[,4],cebpb[,5], cebpb[,6],cebpb[,9]))
+
+
+cebpb <- read.delim("~/Documents/R/fpwm-Thesis-new/RSAT problem/Reverse complemetn/CEBPB JASPAR format (Positive strand).ft", header=FALSE, comment.char="#")
+positive <- data.frame(cbind(cebpb[,4],cebpb[,5], cebpb[,6],cebpb[,9]))
+
+ggplot() +geom_histogram(data = reverse,aes(X3),binwidth = 5,alpha=.2, fill="grey") +
+    geom_freqpoly(data = reverse,aes(X3),binwidth = 5,col="red")+ geom_freqpoly(data = positive,aes(X3),binwidth = 5,col="blue") +theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                                                                                                                                        panel.background = element_blank(), axis.line = element_line(colour = "black"))+geom_density(alpha=.2, fill="#FF6666")+labs(x = "Sequence Start Point")+labs(title = "Comparison between Matrix (blue) and its reverse compliment (red)")+ geom_line(size = 2)
+
+
